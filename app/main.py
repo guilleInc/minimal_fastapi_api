f4d48104047500fc9fcee6335499596e672037db
Guillermo Luis Incatasciato <guillermo.incatasciato@mi.unc.edu.ar>
2026-09-03 15:22:43 -0300

    Add Cors Middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from collections.abc import AsyncIterator
+from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine
 from app.exception_handlers import register_exception_handlers
@@ -10,12 +11,21 @@ from app.routes.router import router
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 register_exception_handlers(app)
 app.include_router(router)
